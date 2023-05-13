f102b36efe054b90df08d7ad4c6a99f8d60ccdbc
Samir Karimov <samir.karimov.01@gmail.com>
2023-05-13 16:09:01 +0300

    Добавлен сам файл проекта с решенной задачей

--- project/Program.cs
+++ project/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void SecondArrayWithIF(string[] arrayfirst, string[] arraysecond)
+{
+    int count = 0;
+    for (int i = 0; i < arrayfirst.Length; i++)
+    {
+    if(arrayfirst[i].Length <= 3)
+        {
+        arraysecond[count] = arrayfirst[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+
+string[] arrayfirst = new string[5] {"123", "23", "hello", "world", "res"};
+string[] arraysecond = new string[arrayfirst.Length];
+PrintArray(arrayfirst);
+SecondArrayWithIF(arrayfirst, arraysecond);
+PrintArray(arraysecond);
